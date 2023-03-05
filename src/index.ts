5305aa815412a21e5ba57dc74c8779249cb17cbf
Miles Smith <miles@helius.xyz>
2023-03-05 12:28:58 -0500

    fix: remove header policy

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,20 @@ interface Env {
 	HELIUS_API_KEY: string;
 }
 
+// If the request is an OPTIONS request, return a 200 response with permissive CORS headers
+// This is required for the Helius RPC Proxy to work from the browser and arbitrary origins
+// If you wish to restrict the origins that can access your Helius RPC Proxy, you can do so by
+// changing the `*` in the `Access-Control-Allow-Origin` header to a specific origin.
+// For example, if you wanted to allow requests from `https://example.com`, you would change the
+// header to `https://example.com`.
 const corsHeaders = {
 	"Access-Control-Allow-Origin": "*",
 	"Access-Control-Allow-Methods": "GET, HEAD, POST, PUT, OPTIONS",
-	"Access-Control-Allow-Headers": "Content-Type",
 }
 
 export default {
 	async fetch(request: Request, env: Env) {
 
-		// If the request is an OPTIONS request, return a 200 response with permissive CORS headers
-		// This is required for the Helius RPC Proxy to work from the browser and arbitrary origins
-		// If you wish to restrict the origins that can access your Helius RPC Proxy, you can do so by
-		// changing the `*` in the `Access-Control-Allow-Origin` header to a specific origin.
-		// For example, if you wanted to allow requests from `https://example.com`, you would change the
-		// header to `https://example.com`.
 		if (request.method === "OPTIONS") {
 			return new Response(null, {
 				status: 200,
