97ae7e34df9f8cfcf6f102521750b7aa9aa03f15
Pepi <pepimartinez@protonmail.com>
2025-03-19 18:33:34 -0400

    dedicated

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ interface Env {
 
 export default {
 	async fetch(request: Request, env: Env) {
-
 		// If the request is an OPTIONS request, return a 200 response with permissive CORS headers
 		// This is required for the Helius RPC Proxy to work from the browser and arbitrary origins
 		// If you wish to restrict the origins that can access your Helius RPC Proxy, you can do so by
@@ -15,47 +14,56 @@ export default {
 		// originated from one of the domains in the `CORS_ALLOW_ORIGIN` environment variable.
 		const supportedDomains = env.CORS_ALLOW_ORIGIN ? env.CORS_ALLOW_ORIGIN.split(',') : undefined;
 		const corsHeaders: Record<string, string> = {
-			"Access-Control-Allow-Methods": "GET, HEAD, POST, PUT, OPTIONS",
-			"Access-Control-Allow-Headers": "*",
-		}
+			'Access-Control-Allow-Methods': 'GET, HEAD, POST, PUT, OPTIONS',
+			'Access-Control-Allow-Headers': '*',
+		};
 		if (supportedDomains) {
-			const origin = request.headers.get('Origin')
+			const origin = request.headers.get('Origin');
 			if (origin && supportedDomains.includes(origin)) {
-				corsHeaders['Access-Control-Allow-Origin'] = origin
+				corsHeaders['Access-Control-Allow-Origin'] = origin;
 			}
 		} else {
-			corsHeaders['Access-Control-Allow-Origin'] = '*'
+			corsHeaders['Access-Control-Allow-Origin'] = '*';
 		}
 
-		if (request.method === "OPTIONS") {
+		if (request.method === 'OPTIONS') {
 			return new Response(null, {
 				status: 200,
 				headers: corsHeaders,
 			});
 		}
 
-		const upgradeHeader = request.headers.get('Upgrade')
+		const upgradeHeader = request.headers.get('Upgrade');
 
 		if (upgradeHeader || upgradeHeader === 'websocket') {
-			return await fetch(`https://mainnet.helius-rpc.com/?api-key=${env.HELIUS_API_KEY}`, request)
+			return await fetch(
+				`https://overprivileged-peptized-cbnzbqndnj-dedicated.helius-rpc.com/?api-key=${env.HELIUS_API_KEY}`,
+				request
+			);
 		}
 
-
-		const { pathname, search } = new URL(request.url)
+		const { pathname, search } = new URL(request.url);
 		const payload = await request.text();
-		const proxyRequest = new Request(`https://${pathname === '/' ? 'mainnet.helius-rpc.com' : 'api.helius.xyz'}${pathname}?api-key=${env.HELIUS_API_KEY}${search ? `&${search.slice(1)}` : ''}`, {
-			method: request.method,
-			body: payload || null,
-			headers: {
-				'Content-Type': 'application/json',
-				'X-Helius-Cloudflare-Proxy': 'true',
+		const proxyRequest = new Request(
+			`https://${
+				pathname === '/'
+					? 'https://overprivileged-peptized-cbnzbqndnj-dedicated.helius-rpc.com'
+					: 'api.helius.xyz'
+			}${pathname}?api-key=${env.HELIUS_API_KEY}${search ? `&${search.slice(1)}` : ''}`,
+			{
+				method: request.method,
+				body: payload || null,
+				headers: {
+					'Content-Type': 'application/json',
+					'X-Helius-Cloudflare-Proxy': 'true',
+				},
 			}
-		});
+		);
 
 		return await fetch(proxyRequest).then(res => {
 			return new Response(res.body, {
 				status: res.status,
-				headers: corsHeaders
+				headers: corsHeaders,
 			});
 		});
 	},
